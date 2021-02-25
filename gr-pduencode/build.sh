c421f34281a73b119657e356eb8e492796464f14
ethan-ac <ethanaaronclarke@gmail.com>
2021-02-24 17:28:14 -0800

    added make cleaning to build script, moved some files around

--- gr-pduencode/build.sh
+++ gr-pduencode/build.sh
@@ -1,2 +1,22 @@
-#!/bin/bash
-gnome-terminal -e '/bin/bash -c "mkdir build; cd build/; cmake ..; make; make install; ldconfig; exec bash;" ' 
+#!/bin/sh
+# run uninstall target
+if [ -d "build" ]; then
+	cd build
+	sudo make uninstall
+	cd ..
+	sudo rm -rf build
+fi
+
+# manually remove rogue shared objects (not
+# doing this step will not affect the build,
+# but there may be runtime Python import issues)
+sudo rm -f /usr/local/lib/libgnuradio-pduencode.so*
+sudo rm -rf /usr/local/lib/python3/dist-packages/pduencode
+
+# build and install
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
