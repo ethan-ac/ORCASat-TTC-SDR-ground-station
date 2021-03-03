5e8eed2deb464c3d18a1cdd20f03da6a53806c6e
ethan-ac <ethanaaronclarke@gmail.com>
2021-03-02 17:07:40 -0800

    cleaning

--- gr-pduencode/build.sh
+++ gr-pduencode/build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # run uninstall target
-if [ -d "build" ]; then
-	cd build
-	sudo make uninstall
-	cd ..
-	sudo rm -rf build
-fi
+# if [ -d "build" ]; then
+#	cd build
+#	sudo make uninstall
+#	cd ..
+#	sudo rm -rf build
+# fi
 
 # manually remove rogue shared objects (not
 # doing this step will not affect the build,
 # but there may be runtime Python import issues)
-# sudo rm -f /usr/local/lib/libgnuradio-pduencode.so*
-# sudo rm -rf /usr/local/lib/python3/dist-packages/pduencode
+# sudo rm -f /usr/local/lib/libgnuradio-ampkey.so*
+# sudo rm -rf /usr/local/lib/python3/dist-packages/ampkey
 
 # build and install
 mkdir build
